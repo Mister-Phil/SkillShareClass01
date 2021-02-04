a5d5353f5d5b60f8c021fe486ed9aaf66c6c0666
Mister-Phil <72716392+Mister-Phil@users.noreply.github.com>
2021-02-04 02:23:03 -0500

    Changes: Login
    I made my first cutom login!

--- Python_Basics2.0.py
+++ Python_Basics2.0.py
@@ -1,3 +1,7 @@
-import test1
-
-print("Hola")
+#My first custom LOGIN!!!
+user = "Name"
+userlog = input("Your name, please:")
+if userlog == user: #So since 'userlog' is an input, we can set the good answer as the wanted condition...
+    print("Welcome")
+elif userlog != user: #Here we are saying : "if the answer required IS NOT the string 'user' then return wrong"...
+    print("Please try again...")

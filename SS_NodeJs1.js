08d71c19258d54c83f26d4a4a18b0b6e75bc9dc5
Mister-Phil <72716392+Mister-Phil@users.noreply.github.com>
2021-06-25 12:43:26 -0400

    Update
    Many files have been moved around or created.

---
+++ SS_NodeJs1.js
@@ -0,0 +1,8 @@
+/*
+console.log('Hi Am I alive');
+
+Make sure you have node installed. Enable view of the Terminal and Debug Console 
+(go to the View menu and select Terminal). 
+You can press F5 for Debugging or Ctrl-F5 for Run without debugging.
+*/
+

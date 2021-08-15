e5065c6538171955db295cfaf1e62e1c8b1d14f1
Mister-Phil <72716392+Mister-Phil@users.noreply.github.com>
2021-08-15 17:07:46 -0400

    API course
    Working the API course

---
+++ Node.js API docs/SS_API_Customer.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+var schema = mongoose.schema;
+
+var customerSchema = new Schema({
+    ID:{}
+})
--- YTBE_API2.js
+++ YTBE_API2.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const = [
+const courses = [
     {id: 1, name: 'courseA'},
     {id: 2, name: 'courseB'},
     {id: 3, name: 'courseC'}, 
-]
-
+];
+/*
 app.get('/', (req, res) => {
     res.send('I am alive');
 });
@@ -15,9 +15,15 @@ app.get('/api/course', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.get('/api/course/:id', (req, res) => {
-    res.send(req.query);
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+*/
+app.get('/api/courses/:id', (req, res) => {
+    const courses = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not fount');
+    res.send(course);
 });
 
-const port = process.env.PORT || 3000; //17:15 mins
+const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log('Listening on ${port}...'));

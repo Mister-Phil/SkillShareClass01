7d6001cc1478c621ebc19f55858f7b9cb8dff062
Mister-Phil <72716392+Mister-Phil@users.noreply.github.com>
2022-01-29 22:08:13 -0500

    Rust continuation
    Variables

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,4 +1,19 @@
 // My keys = [], {}
 fn main() {
- println!("Hello, can you see me?");  
+ 
+let a=10;
+let b:u32=15;
+let c:f32=5.34;
+let d=3.3;
+let e:bool =false;
+let f='g';
+let g="Can you see me?"; // You will see it.
+
+println!("value of a variable:{}",a);
+println!("value of a variable:{}",b);
+println!("value of a variable:{}",c);
+println!("value of a variable:{}",d);
+println!("Value of the boolean:{}",e);
+println!("The value is the letter:{}",f);
+println!("Your string is:{}",g);
 }
